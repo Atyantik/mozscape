e092d35af5269400f6022536154e55df67e8d7fb
chirag patel <chiragpatel@atyantik.com>
2017-04-12 12:31:28 +0530

    change in UrlMetricsService

--- src/Services/UrlMetricsService.php
+++ src/Services/UrlMetricsService.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Atyantik\Mozscape\Services;
+
+use Atyantik\Mozscape\Utilities\ConnectionUtility;
+
 /**
  *
  * Service class to call the various methods to
@@ -11,56 +14,58 @@ namespace Atyantik\Mozscape\Services;
  * @author SEOmoz
  *
  */
+class URLMetricsService extends AbstractService
+{
+    /**
+     *
+     * This method returns the metrics about a URL or set of URLs.
+     *
+     * @param mixed <array|string> objectURL - if array is passed then batch request
+     * @param string cols
+     * @return response
+     */
+    public function getUrlMetrics($objectURL, $cols)
+    {
 
-class URLMetricsService extends AbstractService {
-	/**
-	 *
-	 * This method returns the metrics about a URL or set of URLs.
-	 *
-	 * @param mixed <array|string> objectURL - if array is passed then batch request
-	 * @param string cols
-	 * @return response
-	 */
-	public function getUrlMetrics($objectURL, $cols) {
+        $urlToFetch = "http://lsapi.seomoz.com/linkscape/url-metrics/";
+        $postParams = null;
 
-		$urlToFetch = "http://lsapi.seomoz.com/linkscape/url-metrics/";
-		$postParams = null;
+        // if passed more then 1 url - pass them through post
+        if (!is_array($objectURL)) {
+            $urlToFetch .= urlencode($objectURL);
+        } else {
+            $postParams = json_encode($objectURL);
+        }
 
-		// if passed more then 1 url - pass them through post
-		if (!is_array($objectURL)) {
-			$urlToFetch .= urlencode($objectURL);
-		} else {
-			$postParams = json_encode($objectURL);
-		}
+        $urlToFetch .= "?" . $this->getAuthenticator()->getAuthenticationStr();
+        $urlToFetch .= "&Cols=" . $cols;
 
-		$urlToFetch .= "?" . $this->getAuthenticator()->getAuthenticationStr();
-		$urlToFetch .= "&Cols=" . $cols;
+        $response = ConnectionUtility::makeRequest($urlToFetch, $postParams);
 
-		$response = ConnectionUtility::makeRequest($urlToFetch, $postParams);
+        $i = $this->getAuthenticator()->getRateLimit();
 
-		$i = $this->getAuthenticator()->getRateLimit();
+        // if request fails retry with exponential backoff
+        if (isset($response['http']) && $response['http'] != "200") {
 
-		// if request fails retry with exponential backoff
-		if (isset($response['http']) && $response['http'] != "200") {
+            do {
+                $i = $i + $i;
 
-			do {
-				$i = $i + $i;
+                // output message (optional)
+                echo "\n\nERROR: HTTP Response Code (" . $response['http'] . ")";
+                echo "\nWaiting $i seconds to retry";
 
-				// output message (optional)
-    		echo "\n\nERROR: HTTP Response Code (" . $response['http'] . ")";
-				echo "\nWaiting $i seconds to retry";
+                sleep($i);
+                $response = ConnectionUtility::makeRequest($urlToFetch, $postParams);
 
-				sleep($i);
-				$response = ConnectionUtility::makeRequest($urlToFetch, $postParams);
+            } while ($response['http'] != "200");
 
-			} while ($response['http'] != "200");
+        }
 
-		}
+        unset($response['http']);
 
-		unset($response['http']);
-
-		return $response;
-	}
+        return $response;
+    }
 
 }
+
 ?>
